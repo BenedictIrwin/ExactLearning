bbc2099fde50f5e105fd702bb30918d184f4fa95
BenedictIrwin <b_irwin@hotmail.co.uk>
2022-03-22 10:23:59 +0000

    Log Derivative

--- ObjectOriented/Disk_Line_Picking/generate_disk_line_picking.py
+++ ObjectOriented/Disk_Line_Picking/generate_disk_line_picking.py
@@ -2,17 +2,17 @@ import numpy as np
 from matplotlib import pyplot as plt
 from mpl_toolkits import mplot3d
 
-import scipy.integrate as integrate
+#import scipy.integrate as integrate
 
-def real_integrand(x,s): return np.real(x**(s-1)*np.exp(-x**2-x))
-def imag_integrand(x,s): return np.imag(x**(s-1)*np.exp(-x**2-x))
-def special_int(s):  
-  r = integrate.quad(real_integrand, 0, np.inf, args=(s))
-  i = integrate.quad(imag_integrand, 0, np.inf, args=(s))
-  #return (r[0]+ 1j*i[0],r[1:],i[1:])  
-  return r[0]+ 1j*i[0], r[1], i[1]
+#def real_integrand(x,s): return np.real(x**(s-1)*np.exp(-x**2-x))
+#def imag_integrand(x,s): return np.imag(x**(s-1)*np.exp(-x**2-x))
+#def special_int(s):  
+#  r = integrate.quad(real_integrand, 0, np.inf, args=(s))
+#  i = integrate.quad(imag_integrand, 0, np.inf, args=(s))
+#  #return (r[0]+ 1j*i[0],r[1:],i[1:])  
+#  return r[0]+ 1j*i[0], r[1], i[1]
 
-vec_int = np.vectorize(special_int)
+#vec_int = np.vectorize(special_int)
 
 keyword = "Disk_Line_Picking"
 
@@ -63,7 +63,12 @@ print(s)
 ## For each moment, get the expectation of s-1 for Mellin Transform
 q = np.mean(np.power(np.expand_dims(lengths,1),s-1),axis=0)
 
-if(True):
+## Also get the expectation of E[logX X^(s-1)] which is the derivative of the fingerprint.
+
+dq = np.mean( np.expand_dims(np.log(lengths), axis = 1) * np.power(np.expand_dims(lengths,1),s-1),axis=0)
+
+## Get the ratio...
+def plot_three_dim(q):
   ax = plt.axes(projection='3d')
   # Data for three-dimensional scattered points
   ax.scatter3D(np.real(s), np.imag(s), np.real(q), c=np.real(q), cmap='Reds');
@@ -82,10 +87,22 @@ if(True):
   ax.set_zlabel('$\log E[x^{s-1}]$')
   plt.show()
 
+if(True):
+  plot_three_dim(q)
+  plot_three_dim(dq)
+  plot_three_dim(dq/q)
+
+
 
 ## Save out exact learning data (complex moments)
 np.save("s_values_{}".format(keyword),s)
 np.save("moments_{}".format(keyword),q)
 np.save("logmoments_{}".format(keyword),np.log(q))
+np.save("derivative_{}".format(keyword),dq)
+np.save("logderivative_{}".format(keyword),dq/q)
+
+
+
+
 #np.save("real_error_{}".format(keyword),qre)
 #np.save("imag_error_{}".format(keyword),qie)
