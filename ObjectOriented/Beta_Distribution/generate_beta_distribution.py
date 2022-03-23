911f316604b9afdc26556a11d75cad45b37bf957
unknown <ben@optibrium.com>
2022-03-23 14:11:15 +0000

    Added Beta Example and updates shift-gamma

---
+++ ObjectOriented/Beta_Distribution/generate_beta_distribution.py
@@ -0,0 +1,108 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from mpl_toolkits import mplot3d
+
+#import scipy.integrate as integrate
+
+#def real_integrand(x,s): return np.real(x**(s-1)*np.exp(-x**2-x))
+#def imag_integrand(x,s): return np.imag(x**(s-1)*np.exp(-x**2-x))
+#def special_int(s):  
+#  r = integrate.quad(real_integrand, 0, np.inf, args=(s))
+#  i = integrate.quad(imag_integrand, 0, np.inf, args=(s))
+#  #return (r[0]+ 1j*i[0],r[1:],i[1:])  
+#  return r[0]+ 1j*i[0], r[1], i[1]
+
+#vec_int = np.vectorize(special_int)
+
+keyword = "Beta_Distribution"
+
+size = 1000000
+
+a = 3
+b = 4
+
+lengths = np.random.beta(a,b,size=size)
+
+## Show a histogram
+plt.hist(lengths, bins = 100, density = True)
+
+from scipy.special import beta
+
+xx = np.linspace(0,1,100)
+ff = xx**(a-1)*(1-xx)**(b-1)/beta(a,b)
+plt.plot(xx,ff,'k-')
+
+#from scipy.stats import chi
+#xx = np.linspace(0,5,100)
+#yy = chi.pdf(xx, 10)
+#plt.plot(xx,yy,'-k')
+
+plt.show()
+
+
+## Generate reail and imaginary part complex moments
+s_size = 5
+s1 = np.random.uniform(low = 1, high = 7, size = s_size)
+s2 = np.random.uniform(low = -1*np.pi, high = 1*np.pi, size = s_size)
+s = np.expand_dims(s1 + s2*1j, axis = 0)
+print(s)
+
+#s = np.array([1+0j,2+0j,3+0j,4+0j, 1.5+1j, 1.5-1j, 2.5+1.5j, 2.5-1.5j])
+
+#q, qre, qie = vec_int(s)
+
+
+
+## For each moment, get the expectation of s-1 for Mellin Transform
+q = np.mean(np.power(np.expand_dims(lengths,1),s-1),axis=0)
+
+from scipy.special import gamma
+
+## Also get the expectation of E[logX X^(s-1)] which is the derivative of the fingerprint.
+
+dq = np.mean( np.expand_dims(np.log(lengths), axis = 1) * np.power(np.expand_dims(lengths,1),s-1),axis=0)
+ddq = np.mean( np.expand_dims(np.log(lengths)**2, axis = 1) * np.power(np.expand_dims(lengths,1),s-1),axis=0)
+dddq = np.mean( np.expand_dims(np.log(lengths)**3, axis = 1) * np.power(np.expand_dims(lengths,1),s-1),axis=0)
+s = s[0]
+
+## Get the ratio...
+def plot_three_dim(q):
+  ax = plt.axes(projection='3d')
+  # Data for three-dimensional scattered points
+  ax.scatter3D(np.real(s), np.imag(s), np.real(q), c=np.real(q), cmap='Reds');
+  ax.scatter3D(np.real(s), np.imag(s), np.imag(q), c=np.imag(q), cmap='Greens');
+  ax.set_xlabel('Re(s)')
+  ax.set_ylabel('Im(s)')
+  ax.set_zlabel('$E[x^{s-1}]$')
+  plt.show()
+  
+  ax = plt.axes(projection='3d')
+  # Data for three-dimensional scattered points
+  ax.scatter3D(np.real(s), np.imag(s), np.real(np.log(q)), c=np.real(np.log(q)), cmap='Reds');
+  ax.scatter3D(np.real(s), np.imag(s), np.imag(np.log(q)), c=np.imag(np.log(q)), cmap='Greens');
+  ax.set_xlabel('Re(s)')
+  ax.set_ylabel('Im(s)')
+  ax.set_zlabel('$\log E[x^{s-1}]$')
+  plt.show()
+
+if(False):
+  plot_three_dim(q)
+  plot_three_dim(dq)
+  plot_three_dim(dq/q)
+
+
+
+## Save out exact learning data (complex moments)
+np.save("s_values_{}".format(keyword),s)
+np.save("moments_{}".format(keyword),q)
+np.save("logmoments_{}".format(keyword),np.log(q))
+np.save("derivative_{}".format(keyword),dq)
+np.save("logderivative_{}".format(keyword),dq/q)
+np.save("logderivative2_{}".format(keyword),ddq/q)
+np.save("logderivative3_{}".format(keyword),ddq/q)
+
+
+
+
+#np.save("real_error_{}".format(keyword),qre)
+#np.save("imag_error_{}".format(keyword),qie)
--- ObjectOriented/ObjectOriented.py
+++ ObjectOriented/ObjectOriented.py
@@ -30,9 +30,25 @@ string_list = "a b c d e f g h i j k l m n o p q r s t u v w x y z".split(" ")
 ## When the logarithmic derivative is taken, we get the extra term, so this can be very general
 ## phi'(s)/phi(s) + zeta'(s)/zeta(s)
 
+###################################################################################################
+## For dddq we have that 2 (dq/q)**3 - 3 (dq/q) * (ddq/q) + (dddq/q) ~ Sum b^3 tetragamma(a + b s)#
+###################################################################################################
 
+## For polynomials we duplicate the higher order term as well..
 
-
+#############################################################################
+## We need to consider how the concept of order works for higher dimensions!
+## Gamma(s1 + s2)Gamma[s1]Gamma[s2] -->
+## phi(s1,s2), phi[',](s1,s2), phi[,'](s1,s2), phi[','](s1,s2)
+## D[ Log[phi[s1,s2]], s1] // FullSimplify
+## PolyGamma[0, s1] + PolyGamma[0, s1 + s2]
+## D[ Log[phi[s1,s2]], s2] // FullSimplify
+## PolyGamma[0, s2] + PolyGamma[0, s1 + s2]
+##
+## This is great at isolating parts
+## D[ Log[phi[s1,s2]], s1,s2] // FullSimplify
+## PolyGamma[1, s1 + s2]
+#############################################################################
 
 ## A dict of integers and rationals
 rationals_dict = {}
@@ -120,23 +136,28 @@ linear_gamma_dict = {
 "logderivative" : "_VAR2_*digamma(_gamma-rational_ + _s_*_gamma-rational_)",
 "logderivativereq" : "from scipy.special import digamma",
 "logderivative2" : "_VAR2_**2*np.trigamma(_gamma-rational_ + _s_*_gamma-rational_)",
-#"logderivative2req" : "from mpmath import psi"
 "logderivative2req" : "from AdvancedFunctions import *"
 }
 
 scale_gamma_dict = {
-        "n" : 1, 
-        "req" : "from scipy.special import gamma", 
-        "logreq" : "from scipy.special import loggamma", 
-        "moment" : "gamma(_s_*_gamma-rational_)", 
-        "logmoment" : "loggamma(_s_*_gamma-rational_)"}
+"n" : 1, 
+"req" : "from scipy.special import gamma", 
+"logreq" : "from scipy.special import loggamma", 
+"moment" : "gamma(_s_*_gamma-rational_)", 
+"logmoment" : "loggamma(_s_*_gamma-rational_)"
+}
 
 shift_gamma_dict = {
-        "n" : 1, 
-        "req" : "from scipy.special import gamma", 
-        "logreq" : "from scipy.special import loggamma", 
-        "moment" : "gamma(_gamma-rational_ + _s_)", 
-        "logmoment" : "loggamma(_gamma-rational_ + _s_)"}
+"n" : 1, 
+"req" : "from scipy.special import gamma", 
+"logreq" : "from scipy.special import loggamma", 
+"moment" : "gamma(_gamma-rational_ + _s_)", 
+"logmoment" : "loggamma(_gamma-rational_ + _s_)",
+"logderivative" : "digamma(_gamma-rational_ + _s_)",
+"logderivativereq" : "from scipy.special import digamma",
+"logderivative2" : "np.trigamma(_gamma-rational_ + _s_)",
+"logderivative2req" : "from AdvancedFunctions import *"
+}
 
 neg_linear_gamma_dict = {
 "n" : 2, 
@@ -160,12 +181,16 @@ neg_scale_gamma_dict = {
         "logmoment" : "-loggamma(_s_*_gamma-rational_)"}
 
 neg_shift_gamma_dict = {
-        "n" : 1, 
-        "req" : "from scipy.special import rgamma", 
-        "logreq" : "from scipy.special import loggamma", 
-        "moment" : "rgamma(_gamma-rational_ + _s_)", 
-        "logmoment" : "-loggamma(_gamma-rational_ + _s_)"}
-
+"n" : 1, 
+"req" : "from scipy.special import rgamma", 
+"logreq" : "from scipy.special import loggamma", 
+"moment" : "rgamma(_gamma-rational_ + _s_)", 
+"logmoment" : "-loggamma(_gamma-rational_ + _s_)",
+"logderivative" : "-digamma(_gamma-rational_ + _s_)",
+"logderivativereq" : "from scipy.special import digamma",
+"logderivative2" : "-np.trigamma(_gamma-rational_ + _s_)",
+"logderivative2req" : "from AdvancedFunctions import *"
+}
 ## We only have one constant and let the prefactor absorb the scaling
 P1_dict = {
         "n" : 1, 
@@ -528,7 +553,7 @@ class ExactEstimator:
       #with open(".\\Functions\\{}.py".format(self.function),"r") as f: flines = f.readlines()
       if('fingerprint' in globals().keys()): del globals()['fingerprint']
       if('logderivative' in globals().keys()): del globals()['logderivative']
-      if('logderivative2' in globals().keys()): del globals()['logderivative']
+      if('logderivative2' in globals().keys()): del globals()['logderivative2']
       
       ## Executes the python in the custom file (i.e. a function)
       with open("./Functions/{}.py".format(self.function),"r") as f: flines = f.readlines()
@@ -551,7 +576,7 @@ class ExactEstimator:
     ## Overwrite any existing function with the name 
     if('fingerprint' in globals().keys()): del globals()['fingerprint']
     if('logderivative' in globals().keys()): del globals()['logderivative']
-    if('logderivative2' in globals().keys()): del globals()['logderivative']
+    if('logderivative2' in globals().keys()): del globals()['logderivative2']
     #with open(".\\Functions\\{}.py".format(key),"r") as f: flines = f.readlines()
     with open("./Functions/{}.py".format(key),"r") as f: flines = f.readlines()
     exec("".join(flines),globals())
@@ -774,162 +799,6 @@ def gen_fpdict(parlist,N='max',mode='first',name=''):
     exit()
   hash_string = ":".join([name,mode,str(N),hash_string])
   return hash_string
-
-
-
-#### Working Example... ####
-EE = ExactEstimator("Simple_Exponential", folder = "Simple_Exponential")
-EE.set_fingerprint( gen_fpdict(['linear-gamma']))
-n_bfgs = 100
-for i in range(n_bfgs): 
-  EE.BFGS(order=2)
-  print("{}%".format(100*(i+1)/n_bfgs),flush=True)
-EE.speculate(samples = 1, k = 4)
-############################
-
-exit()
-
-##
-EE = ExactEstimator("Disk_Line_Picking", folder = "Disk_Line_Picking")
-#EE = ExactEstimator("Chi_Distribution", folder = "Chi_Distribution")
-fps = [gen_fpdict(['c','c^s','linear-gamma','linear-gamma','neg-linear-gamma','neg-linear-gamma'])]
-#fps = [gen_fpdict(['c','c^s','linear-gamma'])]
-for k in fps:
-  print("Setting Fingerprint: ",k)
-  EE.set_fingerprint(k)
-
-  ## Do a bit of a random search
-  ##EE.preseed(1000, logd = True)
-
-  ##exit()
-  n_bfgs = 50
-  for i in range(n_bfgs):
-    EE.BFGS(order = 1)
-    print("{}%".format(100*(i+1)/n_bfgs),flush=True)
-
-print("Completed!")
-EE.speculate(samples = 1, k = 4)
-
-optimal_points = [2/np.pi**(1/4),np.sqrt(2),1,1/2,1,1,2,1,5/2,1/2]
-#optimal_points = [np.sqrt(2**(-7/2)/3),np.sqrt(2**(1/2)),9/2,1/2]
-print(optimal_points)
-print(np.array(optimal_points).shape)
-
-
-print("Theoretical Best!")
-loss = EE.point_evaluation(optimal_points, order = 0)
-print("log phi(s)",loss,optimal_points)
-loss = EE.point_evaluation(optimal_points, order = 1)
-print("D_s log phi(s)",loss,optimal_points)
-loss = EE.point_evaluation(optimal_points, order = 2)
-print("D_s D_s log phi(s)",loss,optimal_points)
-
-plots(EE.s_values,EE.logmoments,fingerprint(*optimal_points))
-plots(EE.s_values,EE.ratio,logderivative(*optimal_points))
-plots(EE.s_values,EE.ratio2-EE.ratio**2,logderivative2(*optimal_points))
-
-print("\n~~~ Numerical Best! ~~~")
-loss = EE.point_evaluation(EE.best_params, order = 0)
-print("log phi(s)",loss,EE.best_params)
-loss = EE.point_evaluation(EE.best_params, order = 1)
-print("D_s log phi(s)",loss,EE.best_params)
-loss = EE.point_evaluation(EE.best_params, order = 2)
-print("D_s D_s log phi(s)",loss,EE.best_params)
-
-pp = EE.best_params
-plots(EE.s_values,EE.logmoments,fingerprint(*pp))
-plots(EE.s_values,EE.ratio,logderivative(*pp))
-plots(EE.s_values,EE.ratio2-EE.ratio**2,logderivative2(*pp))
-
-exit()
-
-
-#########################################
-## Start the code here pointing to a file
-EE = ExactEstimator("Chi_Distribution", folder = "Chi_Distribution")
-
-fps=[
-#gen_fpdict(['c','linear-gamma']),
-#gen_fpdict(['c^s','linear-gamma']),
-gen_fpdict(['c','c^s','linear-gamma']),
-#gen_fpdict(['c','c^s','neg-linear-gamma']),
-#gen_fpdict(['c','c^s','linear-gamma','neg-linear-gamma']),
-#gen_fpdict(['c','c^s','linear-gamma','neg-linear-gamma','linear-gamma']),
-#gen_fpdict(['c','c^s','linear-gamma','neg-linear-gamma','neg-linear-gamma']),
-#gen_fpdict(['c','c^s','linear-gamma','linear-gamma','neg-linear-gamma','neg-linear-gamma']),
-#gen_fpdict(['c','c^s','linear-gamma','linear-gamma','linear-gamma','neg-linear-gamma','neg-linear-gamma','neg-linear-gamma']),
-#gen_fpdict(['c','linear-gamma','linear-gamma','neg-linear-gamma','neg-linear-gamma']),
-#gen_fpdict(['c','linear-gamma','linear-gamma','linear-gamma','neg-linear-gamma','neg-linear-gamma','neg-linear-gamma']),
-#gen_fpdict(['c','c^s','linear-gamma','neg-linear-gamma']),
-#gen_fpdict(['c','c^s','linear-gamma','neg-linear-gamma','P1']),
-#gen_fpdict(['c','c^s','linear-gamma','neg-linear-gamma','P2']),
-#gen_fpdict(['c','c^s','2F1'],N=4),
-#gen_fpdict(['c','c^s','2F1','linear-gamma'],N=4),
-#gen_fpdict(['c','c^s','2F1','linear-gamma','neg-linear-gamma'],N=4)
-]
-#fp_x = gen_fpdict(['c','c^s','G-1-0-1-1'])   ### Look up how to do kwargs properly...
-
-## Looping over fingerprints to see which is best
-for k in fps:
-  print("Setting Fingerprint: ",k)
-  EE.set_fingerprint(k)
-  n_bfgs = 300
-  for i in range(n_bfgs):
-    EE.BFGS()
-    print("{}%".format(100*(i+1)/n_bfgs),flush=True)
-
-print("Completed!")
-
-## Get a few samples
-samples_to_try = EE.speculate(samples = 100, k =4)
-
-## Constrains that normalisation is true (s=1) --> Integral = 1
-for q in samples_to_try:
-  loss = EE.point_evaluation(q)
-  print(q, loss)
-
-
-EE.set_fingerprint(EE.best_fingerprint)
-
-print(EE.best_params)
-#plots(EE.s_values,EE.logmoments,fingerprint(*optimal_params))
-plots(EE.s_values,EE.logmoments,fingerprint(*EE.best_params))
-
-##print(EE.__dict__)
-print(EE.results)
-
-best_loss = EE.point_evaluation(EE.best_params)
-print("Best Param Loss:", best_loss)
-optimal_params = [np.sqrt(2**(-7/2)/3),np.sqrt(2**(1/2)),9/2,1/2]
-best_loss = EE.point_evaluation(optimal_params)
-print("Theoretical Best Loss:", best_loss)
-
-### Consider a routine to enforce certain parameter values...
-
-
-
-
-exit()
-
-
-
-
-
-
-exit()
-EE.set_fingerprint(fp_2)
-for i in range(10): EE.BFGS()
-EE.set_fingerprint(fp_3)
-for i in range(10): EE.BFGS()
-
-res = EE.results
-print(res)
-
-EE.summarise()
-
-EE.set_mode('log')
-
-
 
 
 
