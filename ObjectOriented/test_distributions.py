1d293dbe85d14d732efa25809f87edc00db381d3
Benedict Irwin <ben@charmtx.com>
2023-06-21 20:00:03 +0100

    More examples

--- ObjectOriented/test_distributions.py
+++ ObjectOriented/test_distributions.py
@@ -10,6 +10,7 @@ with open("test_1d_distributions.txt","r") as f:
     min = float(min)
     max = float(max)
     print(name, min, max, python, mma, moments)
+    python = python.replace("[comma]",",") # multi-arg functions
     x = np.linspace(min,max,100)
     f = eval("lambda x :"+ python)
     plt.title(name)
