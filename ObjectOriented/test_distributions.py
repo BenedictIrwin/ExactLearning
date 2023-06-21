58b9427b53f7e4a0f047b67b949d789494d06fd6
Benedict Irwin <ben@charmtx.com>
2023-06-21 17:28:59 +0100

    more examples

--- ObjectOriented/test_distributions.py
+++ ObjectOriented/test_distributions.py
@@ -12,5 +12,8 @@ with open("test_1d_distributions.txt","r") as f:
     print(name, min, max, python, mma, moments)
     x = np.linspace(min,max,100)
     f = eval("lambda x :"+ python)
+    plt.title(name)
+    plt.xlabel("x")
+    plt.ylabel("p(x)")
     plt.plot(x,f(x))
     plt.show()
