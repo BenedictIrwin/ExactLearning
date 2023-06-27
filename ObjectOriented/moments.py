deb55b1dbbba8a9706469c54d16cf703ba29a9f5
Benedict Irwin <ben@charmtx.com>
2023-06-27 17:38:14 +0100

    Updated moments to perform integrations

--- ObjectOriented/moments.py
+++ ObjectOriented/moments.py
@@ -1,21 +1,87 @@
 from dataclasses import dataclass
+import scipy.integrate as integrate
+from scipy.interpolate import interp1d
+import numpy as np
 
 @dataclass
 class MomentsBundle():
     """
     Store the internalised exact learning data, ready for fitting
+    Generates an interpolating function
+    Generates an integration technique that can yeild moments
     """
-    num_dims: int
-    valid_s_domain: ??
-    min_x: [] # a vector of length num_dims
-    max_x: [] # a vector of length num_dims
-    is_distribution: bool
-    interpolating_function: ??? # A scipy function
-    max_moment_power: int # How many moment derivatives to take? 
-    q: 
-    dq:
-    ddq:
-    dddq:
+    def __init__(self, name):
+
+        # Definable variables
+        self.name = name
+        self.num_s_samples = 10
+        self.s_domain = {'Re':[1,3],'Im':[-np.pi,np.pi]}
+        self.max_moment_log_derivative_order = 3
+
+        # Placeholder variables
+        self.num_dims = None
+        self.interpolating_function = None
+
+        # These concepts will be expanded to kth order
+        self.vectorised_integration_function = [None for _ in range(self.max_moment_log_derivative_order)]
+        self.moments = [None for _ in range(self.max_moment_log_derivative_order)]
+        self.real_error_in_moments = [None for _ in range(self.max_moment_log_derivative_order)]
+        self.imaginary_error_in_moments = [None for _ in range(self.max_moment_log_derivative_order)]
+
+    def ingest(self,x,y):
+
+        #TODO: Generalise this
+        self.num_dims = 1
+
+        dimension_1 = True
+        if(dimension_1):
+            self.x_max = np.amax(x)
+            self.x_min = np.amin(x)
+
+        # TODO: idiot_proofing etc.
+
+        # TODO: Determine how likely it is a probability distribution
+
+        # TODO: Understand if y!=0 at x_max and asymptotics
+
+        # Fit interpolating form
+        self.interpolating_function = interp1d(x,y, kind='cubic')
+        #psi_inp_lin = interp1d(x,y)
+
+        for k in range(self.max_moment_log_derivative_order):
+            # Generate integral representation
+            def real_integrand(x,s): return np.real(x**(s-1)*self.interpolating_function(x)*np.log(x)**k)
+            def imag_integrand(x,s): return np.imag(x**(s-1)*self.interpolating_function(x)*np.log(x)**k)
+            def special_int(s):  
+                # N.B. Can use np.inf for exact equations if overriding
+                r = integrate.quad(real_integrand, self.x_min, self.x_max, args=(s))
+                i = integrate.quad(imag_integrand, self.x_min, self.x_max, args=(s))  
+                return r[0]+ 1j*i[0], r[1], i[1]
+            self.vectorised_integration_function[k] = np.vectorize(special_int)
+
+        # TODO: add timing as optional outputs
+
+        # Generate s-domain [once, remains fixed]
+        s1 = np.random.uniform(
+            low = self.s_domain['Re'][0], 
+            high = self.s_domain['Re'][1], 
+            size = self.num_s_samples)
+        s2 = np.random.uniform(
+            low = self.s_domain['Im'][0], 
+            high = self.s_domain['Im'][1], 
+            size = self.num_s_samples)
+        
+        # Array of complex s-values
+        self.complex_s_samples = np.array([ t1 + t2*1j for t1,t2 in zip(s1,s2) ])
+
+        # TODO: determine domain of validity
+
+        # Perform the numerical integration for moments m(s)
+        # Calculate m'(s), m''(s),..., m^{(k)}(s) etc. as  E[log(X)^k X^{s-1} f(x)](s)
+        for k in range(self.max_moment_log_derivative_order):
+            self.moments[k], self.real_error_in_moments[k], self.imaginary_error_in_moments[k] = self.vectorised_integration_function[k](self.complex_s_samples)
+        
+
 
 @dataclass
 class ExactLearningResult():
@@ -29,7 +95,9 @@ class ExactLearningResult():
 
 
 
-def ingest(x,y) -> MomentsBundle:
+
+
+def ingest(x,y,name) -> MomentsBundle:
     """
     Take x and y as plt.plot would
     Interpolate, generate this function as part of moments bundle
@@ -42,3 +110,7 @@ def ingest(x,y) -> MomentsBundle:
     # dig out an example of interpolation (i.e. Schrodinger)
 
     # dig out an example of integration (i.e. moment fitting)
+
+    # name is used as the folder name for this problem,
+    # e.g. "Beta_Distribution"
+    return MomentsBundle(name).ingest(x,y)
